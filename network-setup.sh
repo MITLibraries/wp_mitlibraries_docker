0ced537734eeb91b0010bb1055106ae79453a3f6
Matt Bernhardt <mjbernha@mit.edu>
2020-04-21 23:08:57 -0400

    Better admin account creation

--- network-setup.sh
+++ network-setup.sh
@@ -4,8 +4,7 @@ docker-compose run --rm wpcli core multisite-install \
 	--url="localhost" \
 	--base="/" \
 	--title="MIT Libraries" \
-	--admin_email="mjbernha@mit.edu" \
-	--admin_password="abcd1234"
+	--admin_email="admin@example.org"
 
 # Site 1, the parent site, comes for free
 
